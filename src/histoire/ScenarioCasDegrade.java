6581f2faf118f0c9daa682194de8365730db92ed
ghodbanenourelhouda <gho.nourelhouda@gmail.com>
2024-03-12 00:29:37 +0100

    Partie 3

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -1,14 +1,15 @@
 package histoire;
 import villagegaulois.Etal;
 import personnages.Gaulois;
-
+import villagegaulois.Village;
+import villagegaulois.VillageSansChefException;
 
 public class ScenarioCasDegrade {
 	
 	public static void main(String[] args) {
 		Etal etal = new Etal();
 		etal.libererEtal();
-	
+		Village village = new Village("le village des irréductibles", 10, 5);
 		
 		Gaulois acheteur1 = null ; 
 		etal.acheterProduit(5, acheteur1); 
@@ -18,13 +19,15 @@ public class ScenarioCasDegrade {
 		etal.acheterProduit(-5, acheteur2); 
 		
 		etal.occuperEtal (acheteur2 , "pomme", 30);
+		
+		try {
+            System.out.println(village.afficherVillageois());
+        } catch (VillageSansChefException e) {
+            System.err.println( e.getMessage());
+		
+		
 		System.out.println("Fin du test");
 		
 		}
-	
-		
-		
-		
-		
 
-}
+	} }
